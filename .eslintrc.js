f47e06d6c550f822015f769d923c8a8f51339c05
Jason Hutchens <jasonhutchens@gmail.com>
2017-02-26 20:53:33 +0800

    remove garbage

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,19 +13,14 @@ module.exports = {
     'PROD': true,
     '__THEME': true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
-  // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
     'arrow-parens': 0,
     'one-var': 0,
-    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
+    'brace-style': [2, '1tbs', { 'allowSingleLine': true }]
   }
 }
--- src/components/Error404.vue
+++ src/components/Error404.vue
@@ -25,34 +25,34 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      canGoBack: window.history.length > 1
-    }
-  },
-  methods: {
-    goBack () {
-      window.history.go(-1)
+  export default {
+    data () {
+      return {
+        canGoBack: window.history.length > 1
+      }
+    },
+    methods: {
+      goBack () {
+        window.history.go(-1)
+      }
     }
   }
-}
 </script>
 
 <style lang="stylus">
-.error-page
-  .error-code
-    height 50vh
-    width 100%
-    padding-top 15vh
-    font-size 30vmax
-    color rgba(255, 255, 255, .2)
-    overflow hidden
-  .error-card
-    margin-top -25px
-    width 90vw
-    max-width 600px
-    padding 50px
-    i
-      font-size 5rem
+  .error-page
+    .error-code
+      height 50vh
+      width 100%
+      padding-top 15vh
+      font-size 30vmax
+      color rgba(255, 255, 255, .2)
+      overflow hidden
+    .error-card
+      margin-top -25px
+      width 90vw
+      max-width 600px
+      padding 50px
+      i
+        font-size 5rem
 </style>
