bb143313faf209bb4350e0fc007ae9d49671d0a9
Jason Hutchens <jasonhutchens@gmail.com>
2017-03-16 16:05:40 +0800

    debug view for inputs and actions

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,161 @@
 <template>
   <q-layout>
+    <div slot="header" class="toolbar" v-show="debug">
+      <button class="hide-on-drawer-visible" @click="$refs.leftDrawer.open()">
+        <i>menu</i>
+      </button>
+      <q-toolbar-title class="text-center">
+        Arumaroom
+      </q-toolbar-title>
+      <button class="hide-on-drawer-visible" @click="$refs.rightDrawer.open()">
+        <i>menu</i>
+      </button>
+    </div>
+    <q-drawer ref="leftDrawer" v-show="debug">
+      <div class="toolbar light">
+        <q-toolbar-title>
+          Inputs
+        </q-toolbar-title>
+      </div>
+      <div class="card">
+        <div class="list item-delimiter">
+          <q-collapsible opened icon="thumb_up" label="Left Hand">
+            <div>
+              <dl>
+                <dt>Position</dt>
+                <dd>{{ hands.left.position }}</dd>
+              </dl>
+              <dl>
+                <dt>Rotation</dt>
+                <dd>{{ hands.left.rotation }}</dd>
+              </dl>
+              <dl>
+                <dt>Pose</dt>
+                <dd>{{ hands.left.pose }}</dd>
+              </dl>
+            </div>
+          </q-collapsible>
+          <q-collapsible opened icon="thumb_down" label="Right Hand">
+            <div>
+              <dl>
+                <dt>Position</dt>
+                <dd>{{ hands.right.position }}</dd>
+              </dl>
+              <dl>
+                <dt>Rotation</dt>
+                <dd>{{ hands.right.rotation }}</dd>
+              </dl>
+              <dl>
+                <dt>Pose</dt>
+                <dd>{{ hands.right.pose }}</dd>
+              </dl>
+            </div>
+          </q-collapsible>
+        </div>
+      </div>
+    </q-drawer>
     <div class="layout-view">
       <canvas id="viewport"></canvas>
     </div>
+    <q-drawer right-side ref="rightDrawer" v-show="debug">
+      <div class="toolbar light">
+        <q-toolbar-title>
+          Actions
+        </q-toolbar-title>
+      </div>
+      <div class="card">
+        <div class="list item-delimiter">
+          <q-collapsible opened icon="camera_alt" label="Camera">
+            <div class="list">
+              <div class="item two-lines">
+                <i class="item-primary">arrow_forward</i>
+                <div class="item-content">
+                  <q-range v-model="camera.pan[0]" :min="-1" :max="1"></q-range>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">arrow_upward</i>
+                <div class="item-content">
+                  <q-range v-model="camera.pan[1]" :min="-1" :max="1"></q-range>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">rotate_right</i>
+                <div class="item-content">
+                  <q-range v-model="camera.tilt" :min="-1" :max="1"></q-range>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">zoom_out_map</i>
+                <div class="item-content">
+                  <q-range v-model="camera.zoom" :min="-1" :max="1"></q-range>
+                </div>
+              </div>
+            </div>
+          </q-collapsible>
+          <q-collapsible icon="wb_incandescent" label="Lights">
+            <div class="list">
+              <div class="item two-lines">
+                <i class="item-primary">color_lens</i>
+                <div class="item-content">
+                  <q-select type="list" v-model="lights.colour" :options="lights.colours" placeholder="Colour"></q-select>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">flare</i>
+                <div class="item-content">
+                  <q-select type="list" v-model="lights.pattern" :options="lights.patterns" placeholder="Pattern"></q-select>
+                </div>
+              </div>
+            </div>
+          </q-collapsible>
+          <q-collapsible icon="music_note" label="Music">
+            <div class="list">
+              <div class="item two-lines">
+                <i class="item-primary">queue_music</i>
+                <div class="item-content">
+                  <q-select type="list" v-model="music.track" :options="music.tracks" placeholder="Track"></q-select>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">volume_up</i>
+                <div class="item-content">
+                  <q-range v-model="music.volume" :min="0" :max="1"></q-range>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">equalizer</i>
+                <div class="item-content">
+                  <q-select type="list" v-model="music.filter" :options="music.filters" placeholder="Filter"></q-select>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">whatshot</i>
+                <div class="item-content">
+                  <q-range v-model="music.frequency" :min="20" :max="20000"></q-range>
+                </div>
+              </div>
+              <div class="item two-lines">
+                <i class="item-primary">star</i>
+                <div class="item-content">
+                  <q-range v-model="music.quality" :min="0" :max="100"></q-range>
+                </div>
+              </div>
+            </div>
+          </q-collapsible>
+          <q-collapsible icon="movie_filter" label="Visual">
+            <div class="list">
+              <div class="item two-lines">
+                <i class="item-primary">looks</i>
+                <div class="item-content">
+                  <q-select type="list" v-model="visual.effect" :options="visual.effects" placeholder="Effect"></q-select>
+                </div>
+              </div>
+            </div>
+          </q-collapsible>
+        </div>
+      </div>
+    </q-drawer>
   </q-layout>
 </template>
 
@@ -15,6 +168,92 @@
   export default {
     data () {
       return {
+        debug: true,
+        bogus: 0,
+        hands: {
+          left: {
+            position: [0, 0, 0],
+            rotation: [0, 0, 0],
+            pose: 'open'
+          },
+          right: {
+            position: [0, 0, 0],
+            rotation: [0, 0, 0],
+            pose: 'open'
+          }
+        },
+        camera: {
+          pan: [0, 0],
+          tilt: 0,
+          zoom: 0
+        },
+        lights: {
+          colours: [{
+            label: 'Red',
+            value: 'red'
+          }, {
+            label: 'Green',
+            value: 'green'
+          }, {
+            label: 'Blue',
+            value: 'blue'
+          }],
+          colour: null,
+          patterns: [{
+            label: 'Pulse',
+            value: 'pulse'
+          }, {
+            label: 'Swipe',
+            value: 'swipe'
+          }],
+          pattern: null
+        },
+        music: {
+          tracks: [{
+            label: 'Dubstep',
+            value: 'bensound-dubstep'
+          }, {
+            label: 'Moose',
+            value: 'bensound-moose'
+          }],
+          track: null,
+          volume: 0.5,
+          filters: [{
+            label: 'Lowpass',
+            value: 'lowpass'
+          }, {
+            label: 'Highpass',
+            value: 'highpass'
+          }, {
+            label: 'Bandpass',
+            value: 'bandpass'
+          }, {
+            label: 'Lowshelf',
+            value: 'lowshelf'
+          }, {
+            label: 'Highshelf',
+            value: 'highshelf'
+          }, {
+            label: 'Peaking',
+            value: 'peaking'
+          }, {
+            label: 'Notch',
+            value: 'notch'
+          }, {
+            label: 'Allpass',
+            value: 'allpass'
+          }],
+          filter: null,
+          frequency: 10000,
+          quality: 0
+        },
+        visual: {
+          effects: [{
+            label: 'Hand Trails',
+            value: 'trails'
+          }],
+          effect: null
+        }
       }
     },
     methods: {
